9f2610265e0cf2dae2f5396154a8b0c5ac1830fa
Sofiane Mahdjoubi <somahdjo@microsoft.com>
2017-09-14 16:38:02 +0200

    Updating

--- hyperv.ps1
+++ hyperv.ps1
@@ -1,4 +1,2 @@
-"select disk 2", "offline disk" | diskpart
-Write-Host "Hyper-V is not Enabled. Installing and Enabling Hyper-V..."
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
 Restart-Computer
--- level2vm.ps1
+++ level2vm.ps1
@@ -1,2 +1,3 @@
-try {New-VM -Name $VMName -MemoryStartupBytes 3GB -Generation 1 -Path "C:\Virtual Machines\$VMName" -ErrorAction SilentlyContinue | Add-VMHardDiskDrive -DiskNumber 2}
+"select disk 2", "offline disk" | diskpart
+try {New-VM -Name Level2VM -MemoryStartupBytes 3GB -Generation 1 -Path "C:\Virtual Machines\Level2VM" -ErrorAction SilentlyContinue | Add-VMHardDiskDrive -DiskNumber 2}
 catch {}
