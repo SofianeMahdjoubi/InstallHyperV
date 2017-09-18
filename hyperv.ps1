6b0a14b317e8793953553b7985041fbfe3565fbe
Sofiane Mahdjoubi <somahdjo@microsoft.com>
2017-09-18 12:20:08 +0200

    Update hyperv.ps1

--- hyperv.ps1
+++ hyperv.ps1
@@ -1,2 +1,7 @@
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' `
+  -Argument '-NoProfile -WindowStyle Hidden -command "& "select disk 2", "offline disk" | diskpart; try {New-VM -Name Level2VM -MemoryStartupBytes 3GB -Generation 1 -Path "C:\Virtual Machines\Level2VM" -ErrorAction SilentlyContinue | Add-VMHardDiskDrive -DiskNumber 2} catch {};"'
+$trigger =  New-ScheduledTaskTrigger -Once -AtStartup
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "CreateLevel2VM" -Description "Create Level 2 VM in Hyper-V for troubleshooting"
+
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
 Restart-Computer
