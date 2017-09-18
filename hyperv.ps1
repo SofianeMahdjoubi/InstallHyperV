8934106229ac07ef60ea2999aef788df047bd692
Sofiane Mahdjoubi <somahdjo@microsoft.com>
2017-09-18 16:48:50 +0200

    Update hyperv.ps1

--- hyperv.ps1
+++ hyperv.ps1
@@ -6,10 +6,3 @@ New-VM -Name Level2VM -MemoryStartupBytes 3GB -Generation 1 -Path "C:\Virtual Ma
 Disable-ScheduledTask -TaskName "CreateLevel2VM"
 "@
 $createscript |Out-file $stscriptpath -force
-
-$action = New-ScheduledTaskAction -Execute "Powershell.exe" `
-  -Argument "-NoProfile -WindowStyle Hidden $stscriptpath"
-$trigger =  New-ScheduledTaskTrigger -AtLogOn
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "CreateLevel2VM" -Description "Create Level 2 VM in Hyper-V for troubleshooting"
-Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart 
-Restart-Computer  
