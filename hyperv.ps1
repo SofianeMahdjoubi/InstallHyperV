d2282eae3ce83700b6a151152f55a444d5d904df
Sofiane Mahdjoubi <somahdjo@microsoft.com>
2017-09-21 09:23:50 +0200

    Update hyperv.ps1

--- hyperv.ps1
+++ hyperv.ps1
@@ -3,6 +3,7 @@ $stscriptpath = "$dir\scheduled.ps1"
 $createscript = @"
 Set-Disk -Number 2 -IsOffline `$true
 New-VM -Name Level2VM -MemoryStartupBytes 3GB -Generation 1 -Path "C:\Virtual Machines\Level2VM" | Add-VMHardDiskDrive -DiskNumber 2
+SET-VMProcessor –VMName Level2VM –count 2
 Disable-ScheduledTask -TaskName "CreateLevel2VM"
 "@
 $createscript |Out-file $stscriptpath -force
